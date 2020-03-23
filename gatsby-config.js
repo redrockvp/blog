dd09f9026a4fcc2ef64d9ddfcfb1a90f240c053c
Peter-Ross <peter-ross@greenlightsolutions.co.za>
2020-03-23 20:26:43 +0200

    removed posts that were causing issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,47 +12,49 @@ module.exports = {
     siteUrl: `https://delog-w3layouts.netlify.com/`,
     home: {
       title: `Hi! I'm Delog`,
-      description: `I have been specifically designed to become a digital home for designers and developers, help them build amazing professional looking websites with ease. You don't have to worry about nitty gritty of web hosting services to run a blog and yet take full advantage of CMS to manage content :)`,
+      description: `I have been specifically designed to become a digital home for designers and developers, help them build amazing professional looking websites with ease. You don't have to worry about nitty gritty of web hosting services to run a blog and yet take full advantage of CMS to manage content :)`
     },
     /* W3Layouts domain verification key for contact forms https://my.w3layouts.com/Forms/ */
-    w3l_dom_key: `5e609f7a2d23fCF_Domain_verify` 
+    w3l_dom_key: `5e609f7a2d23fCF_Domain_verify`
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/_data`,
-      },
+        path: `${__dirname}/_data`
+      }
     },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [{
-          resolve: `gatsby-remark-prismjs`,
-          options: {
-            classPrefix: "language-",
-            inlineCodeMarker: null,
-            aliases: {},
-            showLineNumbers: false,
-            noInlineHighlight: false,
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false
+            }
           },
-        },
-        {
-          resolve: 'gatsby-remark-emojis',
-        }],
-      },
+          {
+            resolve: "gatsby-remark-emojis"
+          }
+        ]
+      }
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
         trackingId: "UA-30027142-1",
-        head: true,
+        head: true
       }
     },
-    `gatsby-plugin-sass`, 
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-netlify-cms`,
-  ],
-}
+    `gatsby-plugin-netlify-cms`
+  ]
+};
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,9 +1,8 @@
-import React from "react"
-import {Link} from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-export default (props) => (
-  <nav className="navigation"> 
+export default props => (
+  <nav className="navigation">
     <Link to="/contact">Contact</Link>
   </nav>
-  
-)
+);
